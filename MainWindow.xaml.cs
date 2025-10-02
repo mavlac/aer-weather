1917d0f2f8f60667793cecabf486dc215213c39a
mavlac <martin.x.vlach@gmail.com>
2025-10-02 14:31:02 +0200

    Initial project commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Linq;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Aer
+{
+	public sealed partial class MainWindow : Window
+	{
+		private const int DefaultWindowWidth = 1280;
+		private const int DefaultWindowHeight = 800;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			// Saving and restoring window size and position
+			WindowPlacementManager.Restore(this, DefaultWindowWidth, DefaultWindowHeight);
+			this.Closed += (s, e) => WindowPlacementManager.Save(this);
+
+			// Saving and restoring the nav pane
+			NavView.IsPaneOpen = false; // TODO
+
+			// Update back button and nav highlight when navigation happens
+			ContentFrame.Navigated += ContentFrame_Navigated;
+
+			ContentFrame.Navigate(typeof(MainPage));
+		}
+
+		private void ContentFrame_Navigated(object sender, Microsoft.UI.Xaml.Navigation.NavigationEventArgs e)
+		{
+			// Enable or disable back button
+			NavView.IsBackEnabled = ContentFrame.CanGoBack;
+
+			// Highlight the correct menu item
+			if (e.SourcePageType == typeof(MainPage))
+			{
+				NavView.SelectedItem = NavView.MenuItems
+					.OfType<NavigationViewItem>()
+					.FirstOrDefault(x => (string)x.Tag == MainPage.NavigationViewItemTag);
+			}
+			else if (e.SourcePageType == typeof(AboutPage))
+			{
+				NavView.SelectedItem = NavView.MenuItems
+					.OfType<NavigationViewItem>()
+					.FirstOrDefault(x => (string)x.Tag == AboutPage.NavigationViewItemTag);
+			}
+			else
+			{
+				// Deselect items for other pages, like Settings
+				NavView.SelectedItem = null;
+			}
+		}
+
+		// Handle menu item selection
+		private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+		{
+			if (args.IsSettingsSelected)
+			{
+				ContentFrame.Navigate(typeof(SettingsPage));
+			}
+			else
+			{
+				if (args.SelectedItem is NavigationViewItem selectedItem)
+				{
+					switch (selectedItem.Tag)
+					{
+						case MainPage.NavigationViewItemTag:
+							ContentFrame.Navigate(typeof(MainPage));
+							break;
+						case AboutPage.NavigationViewItemTag:
+							ContentFrame.Navigate(typeof(AboutPage));
+							break;
+					}
+				}
+			}
+		}
+
+		// Handle Back button
+		private void NavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+		{
+			if (ContentFrame.CanGoBack)
+			{
+				ContentFrame.GoBack();
+			}
+		}
+	}
+}
---
+++ WindowPlacementManager.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Windows.Graphics;
+using Windows.Storage;
+using WinRT.Interop;
+
+namespace Aer
+{
+	public static class WindowPlacementManager
+	{
+		public static void Save(Window window)
+		{
+			var appWindow = GetAppWindow(window);
+			if (appWindow == null) return;
+
+			var settings = ApplicationData.Current.LocalSettings;
+			var prefix = window.GetType().Name;
+
+			var pos = appWindow.Position;
+			var size = appWindow.Size;
+
+			settings.Values[$"{prefix}_X"] = pos.X;
+			settings.Values[$"{prefix}_Y"] = pos.Y;
+			settings.Values[$"{prefix}_W"] = size.Width;
+			settings.Values[$"{prefix}_H"] = size.Height;
+
+			// Save maximized state (ignore minimized)
+			settings.Values[$"{prefix}_IsMaximized"] =
+				appWindow.Presenter is OverlappedPresenter o &&
+				o.State == OverlappedPresenterState.Maximized;
+		}
+
+		public static void Restore(Window window, int defaultWidth, int defaultHeight)
+		{
+			var appWindow = GetAppWindow(window);
+			if (appWindow == null) return;
+
+			var settings = ApplicationData.Current.LocalSettings;
+			var prefix = window.GetType().Name;
+
+			// try size + position
+			if (settings.Values.TryGetValue($"{prefix}_W", out var wObj) &&
+				settings.Values.TryGetValue($"{prefix}_H", out var hObj) &&
+				settings.Values.TryGetValue($"{prefix}_X", out var xObj) &&
+				settings.Values.TryGetValue($"{prefix}_Y", out var yObj))
+			{
+				int x = (int)xObj;
+				int y = (int)yObj;
+				int width = (int)wObj;
+				int height = (int)hObj;
+
+				var rect = new RectInt32(x, y, width, height);
+
+				if (IsRectVisible(rect, window))
+				{
+					appWindow.MoveAndResize(rect);
+				}
+				else
+				{
+					appWindow.Resize(new SizeInt32(defaultWidth, defaultHeight));
+				}
+			}
+			else
+			{
+				appWindow.Resize(new SizeInt32(defaultWidth, defaultHeight));
+			}
+
+			// try maximized
+			if (settings.Values.TryGetValue($"{prefix}_IsMaximized", out var maxObj) &&
+				maxObj is bool isMaximized &&
+				isMaximized &&
+				appWindow.Presenter is OverlappedPresenter overlapped)
+			{
+				overlapped.Maximize();
+			}
+		}
+
+		private static AppWindow? GetAppWindow(Window window)
+		{
+			var hwnd = WindowNative.GetWindowHandle(window);
+			var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
+			return AppWindow.GetFromWindowId(windowId);
+		}
+
+		private static bool IsRectVisible(RectInt32 rect, Window window)
+		{
+			var hwnd = WindowNative.GetWindowHandle(window);
+			var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
+
+			// Get the display that contains (or would contain) the window
+			var display = DisplayArea.GetFromWindowId(windowId, DisplayAreaFallback.Primary);
+			var bounds = display.OuterBounds;
+
+			// check overlap
+			return rect.X < bounds.X + bounds.Width &&
+				   rect.X + rect.Width > bounds.X &&
+				   rect.Y < bounds.Y + bounds.Height &&
+				   rect.Y + rect.Height > bounds.Y;
+		}
+	}
+}
