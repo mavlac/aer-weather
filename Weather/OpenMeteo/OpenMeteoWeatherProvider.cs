63f0e724c2a452d6ebe466cef50aec2981caf367
mavlac <martin.x.vlach@gmail.com>
2025-12-06 20:53:05 +0100

    Provider names

--- Weather/OpenMeteo/OpenMeteoWeatherProvider.cs
+++ Weather/OpenMeteo/OpenMeteoWeatherProvider.cs
@@ -16,7 +16,7 @@ namespace Aer.Weather.OpenMeteo
 		public const int ProviderStaticId = 0;
 		public override int ProviderId => ProviderStaticId;
 		public override string ProviderName => "Open-Meteo";
-		public override string ProviderDescription => "Global coverage";
+		public override string ProviderDescription => "Fast global coverage";
 
 		public override async Task<(WeatherResult? weatherResult, string errorMessage)> GetWeatherAsync(double latitude, double longitude, CancellationToken cancellationToken)
 		{
