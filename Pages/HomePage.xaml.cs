f01db78ad90852c6ff38e164d26449c458509b9a
mavlac <martin.x.vlach@gmail.com>
2026-04-28 10:02:14 +0200

    Dropped char for ellipsis, replaced with ..., was not rendered

--- Pages/HomePage.xaml.cs
+++ Pages/HomePage.xaml.cs
@@ -143,7 +143,7 @@ namespace Aer
 				BadgeNotificationManager.Current.SetBadgeAsGlyph(BadgeNotificationGlyph.Activity);
 				
 				LoadingOverlay.Visibility = Visibility.Visible;
-				LastUpdateTimeText.Text = $"Loading from {WeatherProvider.Get(Preferences.WeatherProviderId).ProviderURL}…";
+				LastUpdateTimeText.Text = $"Loading from {WeatherProvider.Get(Preferences.WeatherProviderId).ProviderURL}...";
 			}
 
 			// Await completion (still necessary to observe exceptions etc.)
